5566054365d8764aa196f8f7d853e7e803b9fe65
b1gbroth3r <55855522+b1gbroth3r@users.noreply.github.com>
2020-10-21 17:27:41 -0500

    Changed to modify sites-enabled and added cert creation for www

--- scripts/apache_document_root_change.sh
+++ scripts/apache_document_root_change.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 certbot --apache --preferred-challenges http --register-unsafely-without-email --agree-tos -d SETUP_DOMAIN_VARIABLE -n --redirect # obtain SSL cert for domain
-sed -i 's/\/var\/www\/html/\/var\/www\/html\/SETUP_WEBROOT_DIR/g' /etc/apache2/sites-enabled/000-default.conf # change document root to /var/www/html/domain/
-sed -i 's/\/var\/www\/html/\/var\/www\/html\/SETUP_WEBROOT_DIR/g' /etc/apache2/sites-enabled/000-default-le-ssl.conf # same as previous line only for HTTPS site
+certbot --apache --preferred-challenges http --register-unsafely-without-email --agree-tos -d SETUP_DOMAIN_VARIABLE,www.SETUP_DOMAIN_VARIABLE -n --redirect # 
+sed -i 's/\/var\/www\/html/\/var\/www\/html\/SETUP_WEBROOT_DIR/g' /etc/apache2/sites-available/000-default.conf # change document root to /var/www/html/domain/
+sed -i 's/\/var\/www\/html/\/var\/www\/html\/SETUP_WEBROOT_DIR/g' /etc/apache2/sites-available/000-default-le-ssl.conf # same as previous line only for HTTPS site
 service apache2 restart
 # your site should now be accessible over HTTP and HTTPS, with a valid certificate on the HTTPS site.
 # We can't run this during the terraform deployment due to DNS records not being set yet, plus time for DNS to propagate is usually necessary.
