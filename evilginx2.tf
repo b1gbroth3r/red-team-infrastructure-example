35894ae6142c893c3a0e3dc83013b555322701ea
b1gbroth3r <55855522+b1gbroth3r@users.noreply.github.com>
2020-11-18 12:11:44 -0600

    Added evilginx2 server deployment

---
+++ evilginx2.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_droplet" "evilginx2" {
+  image    = "debian-9-x64"
+  name     = "evilginx2"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.SETUP_SSH_KEY_NAME.id]
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = file(var.private_ssh_key)
+    timeout     = "60s"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export DEBIAN_FRONTEND=noninteractive",
+      "wget https://github.com/kgretzky/evilginx2/releases/download/2.4.0/evilginx-linux-amd64.tar.gz",
+      "tar zxvf evilginx-linux-amd64.tar.gz",
+      "chmod 700 evilginx/install.sh",
+      "evilginx/install.sh"
+    ]
+  }
+}
